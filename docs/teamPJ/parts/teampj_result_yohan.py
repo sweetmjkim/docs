16edcb4ba50213174ee41d2e9d1748f99da72b5e
entangelk <kdtyohan@gmail.com>
2023-12-19 15:39:42 +0900

    update

--- docs/teamPJ/parts/teampj_result_yohan.py
+++ docs/teamPJ/parts/teampj_result_yohan.py
@@ -3,7 +3,14 @@ from pymongo import MongoClient
 
 class Quest_result:
 
-    def __init__(self) -> None:
+    def __init__(self,address,db,collection1,collection2,collection3,collection4) -> None:
+        self.address=address
+        self.db=db
+        self.collection1=collection1
+        self.collection2=collection2
+        self.collection3=collection3
+        self.collection4=collection4
+
         # self.struct_answer_list=[]
         self.struct_correct_list=[]
         self.struct_score_list=[]
@@ -16,22 +23,28 @@ class Quest_result:
         # self.struct_answerID_list=[]
         pass
 
-    def connect_mongo(self,address,DB,collection1,collection2,collection3,collection4):
-        mongoclient = MongoClient(address)
-        db_local = mongoclient[DB]
-        self.QI_collection = db_local[collection1]
-        self.QA_collection = db_local[collection2]
-        self.NI_collection = db_local[collection3]
-        self.AI_collection = db_local[collection4]
+    def connect_mongo(self):
+        mongoclient = MongoClient(self.address)
+        db_local = mongoclient[self.db]
+        self.QI_collection = db_local[self.collection1]
+        self.QA_collection = db_local[self.collection2]
+        self.NI_collection = db_local[self.collection3]
+        self.AI_collection = db_local[self.collection4]
         return
 
     def get_answer(self):
         get_correct_list=list(self.QA_collection.find({},{})) #점수랑 정답 문제id 가져오기
         for i in range(len(get_correct_list)):
-            self.struct_correct_list.append(get_correct_list[i]['덕재님 정답 필드이름']) #정답만 들어있는 리스트 저장
-            self.struct_score_list.append(get_correct_list[i]['덕재님 점수값 필드이름'])  #점수만 들어있는 리스트 저장
-            self.struct_question_list.append(get_correct_list[i]['덕재님 문제id 필드이름'])
-        print("각 문항 정답 : {}".format(self.struct_correct_list['덕재님 정답 필드이름']))
+            self.struct_correct_list.append(get_correct_list[i]['정답']) #정답만 들어있는 리스트 저장
+            self.struct_score_list.append(get_correct_list[i]['점수'])  #점수만 들어있는 리스트 저장
+            self.struct_question_list.append(get_correct_list[i]['_id'])
+        print("각 문항 정답 : ", end="")
+        for i in range(len(self.struct_correct_list)):
+            if i == len(self.struct_correct_list)-1:
+                print("{}".format(self.struct_correct_list[i]))
+                pass
+            else:
+                print("{},".format(self.struct_correct_list[i]), end="")
         pass
         return
 
@@ -44,7 +57,7 @@ class Quest_result:
         return
 
     def cal_result(self):
-        self.get_answer_list=self.AI_collection.find({},{}) #이름id, 문제id, 입력값 가져오기
+        self.get_answer_list=list(self.AI_collection.find({},{})) #이름id, 문제id, 입력값 가져오기
         # for i in range(len(get_answer_list)):
         #     self.struct_answer_list.append(get_answer_list[i][명준님 이름id 필드이름,명준님 정답 필드이름]) #입력만 들어있는 리스트 저장
         #     # self.struct_answerID_list.append(get_answer_list[i][명준님 이름id 필드이름])
@@ -53,11 +66,14 @@ class Quest_result:
         for i in range(len(self.get_answer_list)):
             for j in range(len(self.name_list)):
                 if self.get_answer_list[j]['명준님 AI 이름id'] in self.name_list[i]['명준님 NI 이름ID']:
-                    count_index.append(j)
+                    count_index.append(self.name_list[j]['명준님 AI 문제id'])
                     pass
+                pass
+            pass
+        # for i in range():
 
                 
-# AI에서 가져온 이름id와 NI에서 가져온 이름 id 비교
+    # AI에서 가져온 이름id와 NI에서 가져온 이름 id 비교
             # 비교한 내용에서 AI의 문제id와 QA에서의 문제id 비교
             # 비교한 내용에서 AI의 입력값과 QA에서의 정답값 비교
             # 비교한 내용에서 정답이면 QA의 점수값 출력 후 덧셈
@@ -75,3 +91,7 @@ class Quest_result:
 
 
 runProgram=Quest_result('mongodb://192.168.0.92:27017',"local",'Quiz_insert','Quiz_answer','Name_input','Answer_input')
+runProgram.connect_mongo()
+runProgram.get_answer()
+runProgram.get_name()
+runProgram.cal_result()
