431c32cb7a8cfbcd9e29c00d08be3ad35863e2b1
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-20 10:58:09 +0900

    update

--- docs/teamPJ/merge_project.py
+++ docs/teamPJ/merge_project.py
@@ -1,11 +1,9 @@
 import parts.quiz_input_dj
-parts.quiz_input_dj.input_questions()
-
 import parts.Name_myungjun
-parts.Name_myungjun.quiz_program()
-
 from parts.teampj_result_yohan import Quest_result
 
+parts.quiz_input_dj.input_questions()
+parts.Name_myungjun.quiz_program()
 runProgram=Quest_result('mongodb://192.168.0.92:27017',"local",'Quiz_insert','Quiz_answer','Name_input','Answer_input')
 runProgram.cal_data()
 
