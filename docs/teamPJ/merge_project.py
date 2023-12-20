b07086e3988265febac768f288171b729472dcdb
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-20 10:59:01 +0900

    update

--- docs/teamPJ/merge_project.py
+++ docs/teamPJ/merge_project.py
@@ -2,8 +2,8 @@ import parts.quiz_input_dj
 import parts.Name_myungjun
 from parts.teampj_result_yohan import Quest_result
 
-parts.quiz_input_dj.input_questions()
-parts.Name_myungjun.quiz_program()
+parts.quiz_input_dj.input_questions() #퀴즈 내용 인풋
+parts.Name_myungjun.quiz_program()  #이용자 퀴즈 풀기
 runProgram=Quest_result('mongodb://192.168.0.92:27017',"local",'Quiz_insert','Quiz_answer','Name_input','Answer_input')
-runProgram.cal_data()
+runProgram.cal_data()   #시험 결과 출력
 
