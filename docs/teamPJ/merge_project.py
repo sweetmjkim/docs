7bc529480f0e80a791492722e6be4f694d7c6077
DeokJae Kim <kdj0712@gmail.com>
2023-12-20 18:55:51 +0900

    fix

--- docs/teamPJ/merge_project.py
+++ docs/teamPJ/merge_project.py
@@ -1,6 +1,12 @@
 import parts.quiz_input_dj
 import parts.Name_myungjun
+from  parts.quiz_input_dj import Connect
 
+database = Connect()
+Quiz_insert = database["Quiz_insert"]
+Quiz_answer = database["Quiz_answer"]
+Name_input = database["Name_input"]         
+Answer_input = database["Answer_input"]
 
 parts.quiz_input_dj.input_questions() #퀴즈 내용 인풋
 parts.Name_myungjun.quiz_program()  #이용자 퀴즈 풀기
